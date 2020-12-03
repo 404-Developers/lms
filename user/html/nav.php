6b3c992a9abade833ba6a054533525fb5aeaabb6
Jihani-31 <jihaninanayakkara@gmail.com>
2020-12-04 02:31:22 +0530

    completed login,signup,bookpage

---
+++ user/html/nav.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<header>
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+
+<div class="logo">
+      <img src="../images/logo.jpg">
+      <h1 style="color: white;">LIBRARY MANAGEMENT SYSTEM</h1>
+      <!-- <h1 style="color: white; font-size: 25px;word-spacing: 10px; line-height: 80px;margin-top: 20px;">ONLINE LIBRARY MANAGEMENT SYSTEM</h1> -->
+    </div>
+
+      <nav>
+        <ul>
+          <li><a href="index.php">HOME</a></li>
+          <li><a href="books.php">BOOKS</a></li>
+          <li><a href="stdLogin.php">USER-LOGIN</a></li>
+          <li><a href="reg.php">SIGN-UP-</a></li>
+          <!-- <li><a href="">FEEDBACK</a></li> -->
+        </ul>
+      </nav>
+</header>
